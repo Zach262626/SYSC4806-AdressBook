902b35ae7310f2164a5d1007f6d240520d7d2192
Zach262626 <zachgallant26@gmail.com>
2025-10-06 13:24:32 -0400

    created home screen

--- src/main/java/mylabs/app/web/AddressBookViewController.java
+++ src/main/java/mylabs/app/web/AddressBookViewController.java
@@ -1,5 +1,6 @@
 package mylabs.app.web;
 
+import mylabs.app.AddressBook;
 import mylabs.app.AddressBookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +16,23 @@ public class AddressBookViewController {
         this.books = books;
     }
 
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("addressBooks", books.findAll());
+        return "home";
+    }
+
     // Example: http://localhost:8080/addressbooks/1/view
     @GetMapping("/{id}/view")
     public String view(@PathVariable Long id, Model model) {
         model.addAttribute("book", books.findById(id).orElse(null));
         return "addressbook";
     }
+
+    @PostMapping
+    public String create(String name) {
+        books.save(new AddressBook(name));
+        return "redirect:/addressbooks";
+    }
+
 }
