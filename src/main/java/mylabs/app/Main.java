02c39e182786277cb3c47b1ddd2170e83389cbf5
Zach262626 <zachgallant26@gmail.com>
2025-10-22 19:04:40 -0400

    made some changed, changed phone from long to String variable si it easier. added a way to add buddies to address books (not working yet)

--- src/main/java/mylabs/app/Main.java
+++ src/main/java/mylabs/app/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         AddressBook book = new AddressBook("Personal");
 
-        BuddyInfo buddy1 = new BuddyInfo("Zach", "50 ave Blue, Campbellton, NB", 5069874000L);
-        BuddyInfo buddy2 = new BuddyInfo("Alice", "123 King St", 5551111L);
+        BuddyInfo buddy1 = new BuddyInfo("Zach", "50 ave Blue, Campbellton, NB", "5069874000");
+        BuddyInfo buddy2 = new BuddyInfo("Alice", "123 King St", "5551111");
 
         // Correct: adding BuddyInfo objects
         book.addBuddy(buddy1);
